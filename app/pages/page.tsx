4868a02a7eaff1e0d57c3d279f3a877381e1ba3a
Bivek10 <bibekkarki662@gmail.com>
2023-09-26 14:16:01 +0545

    next js page setup

---
+++ app/pages/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #de3333;
+  height: 100vh;
+`;
+
+const HomePage = () => {
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
+};
+
+export default HomePage;
